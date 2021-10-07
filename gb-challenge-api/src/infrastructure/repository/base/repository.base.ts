718c116f6aadfb5f30f1da2c2860debbb0688902
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-07 18:10:26 -0300

    feat: adds base repository with common calls across repositories

---
+++ gb-challenge-api/src/infrastructure/repository/base/repository.base.ts
@@ -0,0 +1,35 @@
+import { FilterQuery, Model } from 'mongoose';
+
+export class BaseRepository<Document, Schema> {
+  constructor(protected readonly _model: Model<Document>) {}
+
+  async create(item: Schema): Promise<Document> {
+    return this._model.create(item);
+  }
+
+  async find(filter: FilterQuery<Document>): Promise<Document[]> {
+    return this._model.find(filter);
+  }
+
+  async findOne(filter: FilterQuery<Document>): Promise<Document> {
+    return this._model.findOne(filter);
+  }
+
+  async updateOne(
+    filter: FilterQuery<Document>,
+    item: Schema,
+  ): Promise<Document> {
+    return this._model.findOneAndUpdate(filter, item, {
+      new: true,
+    });
+  }
+
+  async deleteOne(filter: FilterQuery<Document>): Promise<void> {
+    await this._model.findOneAndDelete(filter);
+  }
+
+  async checkExists(filter: FilterQuery<Document>): Promise<boolean> {
+    const result = await this._model.findOne(filter);
+    return !!result;
+  }
+}
--- gb-challenge-api/src/infrastructure/repository/dealer.repository.ts
+++ gb-challenge-api/src/infrastructure/repository/dealer.repository.ts
@@ -1,27 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Model } from 'mongoose';
+import { Model } from 'mongoose';
 import { Dealer, DealerDocument } from '../schema/dealer.schema';
+import { BaseRepository } from './base/repository.base';
 
 @Injectable()
-export class DealerRepository {
+export class DealerRepository extends BaseRepository<DealerDocument, Dealer> {
   constructor(
-    @InjectModel(Dealer.name) private readonly _model: Model<DealerDocument>,
-  ) {}
-
-  async create(dealer: Dealer): Promise<Dealer> {
-    return new Promise<Dealer>(async (resolve, reject) => {
-      try {
-        const result: DealerDocument = await this._model.create(dealer);
-        return resolve(result.toObject());
-      } catch (err) {
-        return reject(err);
-      }
-    });
-  }
-
-  async checkExists(filter: FilterQuery<DealerDocument>): Promise<boolean> {
-    const result = await this._model.findOne(filter);
-    return !!result;
+    @InjectModel(Dealer.name) protected readonly _model: Model<DealerDocument>,
+  ) {
+    super(_model);
   }
 }
