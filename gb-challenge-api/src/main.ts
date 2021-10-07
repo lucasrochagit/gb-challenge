b2cbce250b192b5dadb600e1f934708ae8508a35
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-07 12:12:26 -0300

    feat: add global pipes and filters

--- gb-challenge-api/src/main.ts
+++ gb-challenge-api/src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './config/module/app.module';
+import { AllExceptionsFilter } from './config/filter/exception.filter';
 
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(PORT);
 }
 bootstrap();
