518101d41b150948e99dc5d500adb78eb7a68e04
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-08 13:02:55 -0300

    feat(dealer): update module config

--- gb-challenge-api/src/config/module/dealer.module.ts
+++ gb-challenge-api/src/config/module/dealer.module.ts
@@ -5,18 +5,21 @@ import { DealerService } from '../../business/service/dealer.service';
 import { DealerRepository } from '../../infrastructure/repository/dealer.repository';
 import {
   Dealer,
-  DealerSchema,
+  DealerSchema
 } from '../../infrastructure/schema/dealer.schema';
 import { DealerController } from '../../presentation/controller/dealer.controller';
+import { DealerPasswordController } from '../../presentation/controller/dealer.password.controller';
 import { DealerDTOMapper } from '../../presentation/mapper/dealer.dto.mapper';
+import { DealerPasswordDTOMapper } from '../../presentation/mapper/dealer.password.dto.mapper';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Dealer.name, schema: DealerSchema }]),
   ],
-  controllers: [DealerController],
+  controllers: [DealerController, DealerPasswordController],
   providers: [
     DealerDTOMapper,
+    DealerPasswordDTOMapper,
     DealerService,
     DealerRepository,
     DealerModelMapper,
