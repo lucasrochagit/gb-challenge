aa08ac3f9f3f2cb3fa638d8796005b24eec3e5c1
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-08 13:00:56 -0300

    feat(dealer): update presentation layer

--- gb-challenge-api/src/presentation/controller/dealer.controller.ts
+++ gb-challenge-api/src/presentation/controller/dealer.controller.ts
@@ -1,7 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { DealerModel } from '../../business/model/dealer.model';
 import { DealerService } from '../../business/service/dealer.service';
-import { CreateDealerDTO, DealerDTO } from '../dto/dealer.dto';
+import {
+  CreateDealerDTO,
+  DealerDTO,
+  MethodByIdDealerDTO,
+  UpdateDealerDTO,
+} from '../dto/dealer.dto';
 import { DealerDTOMapper } from '../mapper/dealer.dto.mapper';
 
 @Controller('dealers')
@@ -16,5 +31,36 @@ export class DealerController {
     const model: DealerModel = this._mapper.deserialize(dto);
     const result: DealerModel = await this._service.create(model);
     return this._mapper.serialize(result);
+  }
+
+  @Get()
+  async find(): Promise<DealerDTO[]> {
+    const result: DealerModel[] = await this._service.find();
+    return result.map((item: DealerModel) => this._mapper.serialize(item));
+  }
+
+  @Get(':dealer_id')
+  async findById(@Param() params: MethodByIdDealerDTO): Promise<DealerDTO> {
+    const result: DealerModel = await this._service.findById(params.dealer_id);
+    return this._mapper.serialize(result);
+  }
+
+  @Put(':dealer_id')
+  async update(
+    @Param() params: MethodByIdDealerDTO,
+    @Body() dto: UpdateDealerDTO,
+  ): Promise<DealerDTO> {
+    const model: DealerModel = this._mapper.deserialize(dto);
+    const result: DealerModel = await this._service.update(
+      params.dealer_id,
+      model,
+    );
+    return this._mapper.serialize(result);
+  }
+
+  @Delete(':dealer_id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteById(@Param() params: MethodByIdDealerDTO): Promise<void> {
+    return this._service.deleteById(params.dealer_id);
   }
 }
--- gb-challenge-api/src/presentation/dto/dealer.dto.ts
+++ gb-challenge-api/src/presentation/dto/dealer.dto.ts
@@ -1,18 +1,22 @@
 import {
-    IsDefined,
-    IsEmail,
-    IsNotEmpty, IsString,
-    Matches,
-    MaxLength,
-    MinLength
+  Equals,
+  IsDefined,
+  IsEmail,
+  IsMongoId,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+  ValidateIf,
 } from 'class-validator';
 import { IsAlphaWithSpaces } from '../validator/is.alpha.with.spaces';
 import { IsValidCPF } from '../validator/is.valid.cpf';
-import { DTO } from './dto';
+import { ReadOnlyDTO } from './readonly.dto';
 
-export type DealerDTO = CreateDealerDTO;
+export type DealerDTO = CreateDealerDTO & UpdateDealerDTO;
 
-export class CreateDealerDTO extends DTO {
+export class CreateDealerDTO extends ReadOnlyDTO {
   @IsDefined()
   @IsString()
   @IsNotEmpty()
@@ -40,4 +44,41 @@ export class CreateDealerDTO extends DTO {
   @MinLength(8)
   @MaxLength(20)
   password: string;
+}
+
+export class UpdateDealerDTO extends ReadOnlyDTO {
+  @ValidateIf((dto) => dto.full_name !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsAlphaWithSpaces()
+  full_name: string;
+
+  @ValidateIf((dto) => dto.cpf !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^(\d\d\d.\d\d\d.\d\d\d-\d\d)$/, {
+    message: 'cpf must follow the pattern: XXX.XXX.XX-XX',
+  })
+  @IsValidCPF()
+  cpf: string;
+
+  @ValidateIf((dto) => dto.email !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Equals(undefined, {
+    message:
+      'password cannot be updated from here, please use the appropiate endpoint for this action',
+  })
+  password: string;
+}
+
+export class MethodByIdDealerDTO {
+  @IsMongoId()
+  dealer_id: string;
 }
