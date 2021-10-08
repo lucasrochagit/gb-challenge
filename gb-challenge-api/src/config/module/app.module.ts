4c93b0fabe17e849efea6ae749c1a11bf62e4a14
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-08 13:02:28 -0300

    feat(auth): add module config

--- gb-challenge-api/src/config/module/app.module.ts
+++ gb-challenge-api/src/config/module/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from '../../business/service/app.service';
 import { AppController } from '../../presentation/controller/app.controller';
+import { AuthModule } from './auth.module';
 import { DealerModule } from './dealer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    AuthModule,
     DealerModule,
   ],
   controllers: [AppController],
---
+++ gb-challenge-api/src/config/module/auth.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModelMapper } from '../../business/mapper/auth.model.mapper';
+import { AuthService } from '../../business/service/auth.service';
+import { AuthRepository } from '../../infrastructure/repository/auth.repository';
+import { DealerRepository } from '../../infrastructure/repository/dealer.repository';
+import { Auth, AuthSchema } from '../../infrastructure/schema/auth.schema';
+import {
+  Dealer,
+  DealerSchema,
+} from '../../infrastructure/schema/dealer.schema';
+import { AuthController } from '../../presentation/controller/auth.controller';
+import { AuthDTOMapper } from '../../presentation/mapper/auth.dto.mapper';
+import { AuthRequestDTOMapper } from '../../presentation/mapper/auth.request.dto.mapper';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Auth.name, schema: AuthSchema },
+      { name: Dealer.name, schema: DealerSchema },
+    ]),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthDTOMapper,
+    AuthRequestDTOMapper,
+    AuthService,
+    AuthRepository,
+    DealerRepository,
+    AuthModelMapper,
+  ],
+})
+export class AuthModule {}
