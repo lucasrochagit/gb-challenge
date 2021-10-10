d7ee9775ab59314d2dbc1eb2df9eeee162e162ad
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-10 16:09:49 -0300

    chore: change variables declaration to camelCase

--- gb-challenge-api/src/presentation/validator/util/cpf.validator.util.ts
+++ gb-challenge-api/src/presentation/validator/util/cpf.validator.util.ts
@@ -1,23 +1,21 @@
 export class CPFValidatorUtil {
   static isValidCPF(cpf: string): boolean {
     if (!cpf) return false;
-    const only_numbers: string = cpf.replace(/\D+/g, '');
+    const onlyNumbersCPF: string = cpf.replace(/\D+/g, '');
 
-    if (only_numbers.length !== 11) {
+    if (onlyNumbersCPF.length !== 11) {
       return false;
     }
 
-    const [a, b, c, d, e, f, g, h, i, j, k]: number[] = [...only_numbers].map(
-      function (num) {
-        return parseInt(num, 10);
-      },
+    const [a, b, c, d, e, f, g, h, i, j, k]: number[] = [...onlyNumbersCPF].map(
+      (num) => parseInt(num, 10),
     );
 
-    const mod_j: number =
+    const firstDigit: number =
       (a * 10 + b * 9 + c * 8 + d * 7 + e * 6 + f * 5 + g * 4 + h * 3 + i * 2) %
       11;
-    const expect_j: number = mod_j < 2 ? 0 : 11 - mod_j;
-    const mod_k: number =
+    const expectFirstDigit: number = firstDigit < 2 ? 0 : 11 - firstDigit;
+    const secondDigit: number =
       (a * 11 +
         b * 10 +
         c * 9 +
@@ -27,10 +25,10 @@ export class CPFValidatorUtil {
         g * 5 +
         h * 4 +
         i * 3 +
-        expect_j * 2) %
+        expectFirstDigit * 2) %
       11;
-    const expect_k: number = mod_k < 2 ? 0 : 11 - mod_k;
+    const expectSecondDigit: number = secondDigit < 2 ? 0 : 11 - secondDigit;
 
-    return expect_j === j && expect_k === k;
+    return expectFirstDigit === j && expectSecondDigit === k;
   }
 }
