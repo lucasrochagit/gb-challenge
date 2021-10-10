f4409ae7f928a2b19a562c1e9de250bc0e13e465
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-10 16:08:51 -0300

    feat: add application logs

--- gb-challenge-api/src/main.ts
+++ gb-challenge-api/src/main.ts
@@ -1,13 +1,30 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './config/module/app.module';
+import * as morgan from 'morgan';
 import { AllExceptionsFilter } from './config/filter/exception.filter';
+import { AppModule } from './config/module/app.module';
 
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new AllExceptionsFilter());
+  setMiddlewares(app);
   await app.listen(PORT);
 }
+
+function setMiddlewares(app: INestApplication): void {
+  const httpRequestLogger: Logger = new Logger('HTTPRequest');
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.use(
+    morgan(
+      ':remote-addr :remote-user [:date[iso]] ":method :url HTTP/:http-version" ' +
+        ':status :res[content-length] :response-time ms ":referrer" ":user-agent"',
+      {
+        stream: { write: (str: string) => httpRequestLogger.log(str) },
+      },
+    ),
+  );
+}
+
 bootstrap();
