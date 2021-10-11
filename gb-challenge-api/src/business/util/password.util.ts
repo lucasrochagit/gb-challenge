7e940267a5a6c4aa6c7b11a516d485acbe63d2b8
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-11 15:52:06 -0300

    feat: add improvements on business utils

--- gb-challenge-api/src/business/util/password.util.ts
+++ gb-challenge-api/src/business/util/password.util.ts
@@ -2,11 +2,13 @@ import * as bcrypt from 'bcrypt';
 
 export class PasswordUtil {
   public static encryptPassword(text: string): string {
+    if (!text) return undefined;
     const salt = bcrypt.genSaltSync(10);
     return bcrypt.hashSync(text, salt);
   }
 
   public static isSamePassword(text: string, encryptedText: string): boolean {
+    if (!text || !encryptedText) return false;
     return bcrypt.compareSync(text, encryptedText);
   }
 }
--- gb-challenge-api/src/business/util/token.util.ts
+++ gb-challenge-api/src/business/util/token.util.ts
@@ -4,43 +4,40 @@ import { sign, decode } from 'jsonwebtoken';
 export class TokenUtil {
   static generateAccessToken(ownerId: string): string {
     const { JWT_SECRET, JWT_EXPIRATION } = process.env;
+    if (!ownerId) return undefined;
 
-    return sign(
-      {
-        sub: ownerId,
-      },
-      JWT_SECRET,
-      { expiresIn: JWT_EXPIRATION },
-    );
+    try {
+      return sign(
+        {
+          sub: ownerId,
+        },
+        JWT_SECRET,
+        { expiresIn: JWT_EXPIRATION },
+      );
+    } catch (err) {
+      return undefined;
+    }
   }
 
   static generateRefreshToken(): string {
     return randomBytes(32).toString('hex');
   }
 
-  static getTokenPayload(token: string) {
-    try {
-      return decode(token);
-    } catch (err) {
-      return null;
-    }
-  }
-
   static getTokenPayloadSub(token: string): string {
     try {
       const payload = decode(token, { json: true });
-      return payload.sub ?? null;
+      return payload.sub ?? undefined;
     } catch (err) {
-      return null;
+      return undefined;
     }
   }
 
   static getTokenPayloadExp(token: string): number {
     try {
       const payload = decode(token, { json: true });
-      return payload.exp ?? null;
+      return payload.exp ?? undefined;
     } catch (err) {
-      return null;
+      return undefined;
     }
   }
 }
