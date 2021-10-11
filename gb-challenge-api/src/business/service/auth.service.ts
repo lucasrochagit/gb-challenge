b7bc10c18247ab8c52342358ff11f57c1c7760b3
Lucas Rocha <lucascosmorocha@gmail.com>
2021-10-10 22:59:38 -0300

    chore(auth): remove unused imports on service

--- gb-challenge-api/src/business/service/auth.service.ts
+++ gb-challenge-api/src/business/service/auth.service.ts
@@ -1,7 +1,6 @@
 import {
   BadRequestException,
   Injectable,
-  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthRepository } from '../../infrastructure/repository/auth.repository';
